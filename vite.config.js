398f9eed0ae696e71acf922ae57990f45fc85433
Santiago Garcia <72152591+Sgarcia8@users.noreply.github.com>
2024-10-03 01:20:48 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,17 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
+        plugins: [
+            server: {
+        https: true
+      },
+      build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+          input: '/resources/js/app.js',
+        },
+      },
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
