875c17d85cdae939e11d38bb2d1e7447ad3bb829
Santiago Garcia <72152591+Sgarcia8@users.noreply.github.com>
2024-10-03 01:39:20 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,13 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
         plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        vue(),
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+            vue(),
+        ],
+        // Asegúrate de que Vite utilice HTTPS en producción
+        base: mode === 'production' ? 'https://pruebatodo-production.up.railway.app/' : '/',
     ],
 });
