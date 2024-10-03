05b9218f2cd5cfa1779a1e1ba664a1f99aa85d8f
Santiago Garcia <72152591+Sgarcia8@users.noreply.github.com>
2024-10-03 01:06:18 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel & Vue</title>
-    @vite('resources/css/app.css')
 </head>
 <body>
     <div id="app"></div>
